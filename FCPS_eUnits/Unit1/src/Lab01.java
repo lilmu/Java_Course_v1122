84ca2977e03e9740d180ff0ac3068de2422de0d7
lilmu <ephraimpan.92@gmail.com>
2025-02-18 20:16:52 +0800

    Create Lab01.java
    Create Lab01 with lisa and pete.
    Still figuring how to set the initial parameters for pete.

---
+++ FCPS_eUnits/Unit1/src/Lab01.java
@@ -0,0 +1,28 @@
+import edu.fcps.karel2.Display;
+import edu.fcps.karel2.Robot;
+
+
+public class Lab01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Display.openWorld("../maps/school.map");
+		Robot lisa = new Robot();
+      Robot pete = new Robot();
+      
+      karel.move();
+      karel.pickBeeper();
+      karel.move();
+      karel.turnLeft();
+      karel.move();
+      karel.putBeeper();
+      karel.move();
+      karel.turnLeft();
+      karel.turnLeft();
+	}
+
+}
