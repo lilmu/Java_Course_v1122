a9ef100f0f93e0e3e671b97d90254ebb424b712b
lilmu <ephraimpan.92@gmail.com>
2025-02-24 16:03:26 +0800

    Create Lab02.java

---
+++ FCPS_eUnits/Unit1/src/Lab02.java
@@ -0,0 +1,27 @@
+import edu.fcps.karel2.Display;
+import edu.fcps.karel2.Robot;
+
+
+public class Lab02 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Display.openWorld("../maps/first.map");
+		Robot karel = new Robot();
+      
+      karel.move();
+      karel.pickBeeper();
+      karel.move();
+      karel.turnLeft();
+      karel.move();
+      karel.putBeeper();
+      karel.move();
+      karel.turnLeft();
+      karel.turnLeft();
+	}
+
+}
